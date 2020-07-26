d6bf077e75c6d6e2059c1eda92d85affe5fb8fc8
AhmedNasrullah <ahmed.nasrullah@southtravels.com>
2020-07-26 15:15:33 +0400

    Updates

---
+++ precache-manifest.8d0b62490ea69791123a81810913c232.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5dc5f63a0340c33feb8b96987d1ec7",
+    "url": "/covid-react-app%20/index.html"
+  },
+  {
+    "revision": "4c225efaacb4efb1ed3c",
+    "url": "/covid-react-app%20/static/css/main.29d19af9.chunk.css"
+  },
+  {
+    "revision": "d6ba1ff855fabeea16a7",
+    "url": "/covid-react-app%20/static/js/2.9549187a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-react-app%20/static/js/2.9549187a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c225efaacb4efb1ed3c",
+    "url": "/covid-react-app%20/static/js/main.d40cf150.chunk.js"
+  },
+  {
+    "revision": "c9f2f728438bbd1ca2ea",
+    "url": "/covid-react-app%20/static/js/runtime-main.a13ca32f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-react-app%20/static/media/image.d7265326.png"
+  }
+]);
